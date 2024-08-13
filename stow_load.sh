4094e86e6084e2f2b0086a912dad3ff623e87201
Peter Sheehan <petersheehan2003@gmail.com>
2024-08-13 19:40:04 +0100

    fix(stow): make stow script actually work

--- stow_load.sh
+++ stow_load.sh
@@ -12,14 +12,14 @@ stow_target_dir="$HOME"
 # dotfiles directory
 dotfiles_dir="$HOME/.dotfiles"
 
-# get directories (categories) inside dotfiles directory
-dirs=$(find "$dotfiles_dir" -mindepth 1 -maxdepth 1 -type d)
-
 # cd into dotfiles directory
 cd "$dotfiles_dir" || exit
 
 # iterate through directories in dotfiles directory
-for dir in "${dirs[@]}"; do
-	dirname=$(basename "$dir")       # get the base name (e.g. $HOME/.dotfiles/nvim => nvim)
-	stow -t "$stow_target_dir" "$dirname" # stow the directory into the parent of dotfiles dir
+find "$dotfiles_dir" -mindepth 1 -maxdepth 1 -type d | while read -r dir; do
+  dirname=$(basename "$dir") # get the base name (e.g. $HOME/.dotfiles/nvim => nvim)
+  if [ "$dirname" != ".git" ]; then
+    stow -t "$stow_target_dir" "$dirname" # stow the directory into the parent of dotfiles dir
+    echo "Stowing $dirname..."
+  fi
 done
