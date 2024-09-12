3751a6ec1bee3b865d066be8c21aeaafc8d8492c
Peter Sheehan <petersheehan2003@gmail.com>
2024-09-12 14:48:34 +0100

    feat: set up outlined

--- install.sh
+++ install.sh
@@ -1,1 +1,10 @@
-# source ./zsh/.zshenv
+#!/usr/bin/env bash
+
+# make sure important environment variables are set ($DOTFILES, $ZDOTDIR, $XDG_*)
+source ./zsh/zshenv
+
+# config directory to handle apps that do/don't support XDG spec
+# (attempt to use $XDG_CONFIG_HOME, fall back to $HOME/.config)
+export CONFIG="${XDG_CONFIG_HOME:-$HOME/.config}"
+
+. "$DOTFILES/install/install-zsh.sh"
