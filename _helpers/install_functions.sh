3136631f652d5beae3ef70c580a9179f0a86460a
Peter Sheehan <petersheehan2003@gmail.com>
2024-09-12 15:24:14 +0100

    feat: add coloured logging to dot_install function

--- _helpers/install_functions.sh
+++ _helpers/install_functions.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 function dot_install() {
-  # TODO: add coloured logging
+  echo -e "${blue}=> Installing ${yellow}${1} ${blue}config..."
   . "$DOTFILES/_install/install-${1}.sh"
 }
