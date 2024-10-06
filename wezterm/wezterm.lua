d9b3d3a796dfd0a5d57d57c5a6bdb773a49a371c
Peter Sheehan <petersheehan2003@gmail.com>
2024-10-06 21:00:03 +0100

    feat(wezterm): customise tabs and group config better

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,16 +4,60 @@ local mux = wezterm.mux
 
 local config = wezterm.config_builder()
 
+-- Window ---------------------------------------------------------------------
+
+config.window_decorations = "INTEGRATED_BUTTONS"
+config.window_background_opacity = 0.75
+config.macos_window_background_blur = 10
+
+-- launch maximised
+wezterm.on("gui-startup", function(cmd)
+  ---@diagnostic disable: unused-local
+  -- selene: allow(unused_variable)
+  local tab, pane, window = mux.spawn_window(cmd or {})
+  window:gui_window():maximize()
+end)
+
+-- Tabs -----------------------------------------------------------------------
+
 config.hide_tab_bar_if_only_one_tab = true
 
-config.window_decorations = "RESIZE"
+config.window_frame = {
+  font = wezterm.font({
+    family = "FiraCode Nerd Font",
+    weight = "Bold",
+  }),
+  font_size = 13.0,
+}
+
+wezterm.on(
+  "format-tab-title",
+  function(tab, tabs, panes, conf, hover, max_width)
+    local background = "#1f1f29"
+    local foreground = "#808080"
+
+    if tab.is_active then
+      background = "#c29c41"
+      foreground = "#050505"
+    end
+
+    return {
+      { Background = { Color = background } },
+      { Foreground = { Color = foreground } },
+      { Text = " " .. tab.active_pane.title .. " " },
+    }
+  end
+)
+
+-- Colorscheme ----------------------------------------------------------------
 
 -- color_scheme = "rose-pine",
 -- color_scheme = "tokyonight_night",
 config.color_scheme = "Kanagawa (Gogh)"
 
-config.window_background_opacity = 0.75
-config.macos_window_background_blur = 10
+-- Font -----------------------------------------------------------------------
+
+config.font_size = 18
 
 config.font = wezterm.font({
   family = "FiraCode Nerd Font",
@@ -32,22 +76,13 @@ config.font = wezterm.font({
     -- "ss07", -- =~ !~
   },
 })
--- TODO: add backup font
-config.font_size = 18
+
+-- Keys -----------------------------------------------------------------------
 
 config.send_composed_key_when_left_alt_is_pressed = false -- left opt is ALT/META key
-config.send_composed_key_when_right_alt_is_pressed = true -- right opt is normal (so i can type #)
+config.send_composed_key_when_right_alt_is_pressed = true -- right opt is normal (so I can type # on MacOS)
 
 config.disable_default_key_bindings = true
 config.keys = keys.keys
-
--- launch maximised
-wezterm.on("gui-startup", function(cmd)
-  ---@diagnostic disable: unused-local
-  -- selene: allow(unused_variable)
-  -- local tab, pane, window = mux.spawn_window(cmd or {})
-  local _, _, window = mux.spawn_window(cmd or {})
-  window:gui_window():maximize()
-end)
 
 return config
