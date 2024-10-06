83fc7349e99cec103773f1b4647f47caca16a209
Peter Sheehan <petersheehan2003@gmail.com>
2024-10-06 15:29:28 +0100

    feat(wezterm): disable default keybinds and set my own
    This way only bindings I actually use will be set.

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,5 +1,6 @@
 local wezterm = require("wezterm")
 local mux = wezterm.mux
+local act = wezterm.action
 
 local config = wezterm.config_builder()
 
@@ -13,57 +14,95 @@ config.color_scheme = "Kanagawa (Gogh)"
 config.window_background_opacity = 0.75
 config.macos_window_background_blur = 10
 config.font = wezterm.font({
-	family = "FiraCode Nerd Font",
-	-- family = "JetBrainsMono Nerd Font",
-	weight = "Book",
-	harfbuzz_features = {
-		-- "calt=0", -- disable ligatures
-		"zero", -- 0 => zero with dot
-		"ss05", -- @ => better [ AT ] symbol
-		"ss03", -- & => better [ ampersand ] symbol
-		-- "ss09", -- >>= <<= ||= |=
-		-- "cv25", -- .-
-		-- "cv26", -- :-
-		-- "cv32", -- .=
-		-- "cv27", -- []
-		-- "cv28", -- {. .}
-		-- "ss07", -- =~ !~
-	},
+  family = "FiraCode Nerd Font",
+  -- family = "JetBrainsMono Nerd Font",
+  weight = "Book",
+  harfbuzz_features = {
+    -- "calt=0", -- disable ligatures
+    "zero", -- 0 => zero with dot
+    "ss05", -- @ => better [ AT ] symbol
+    "ss03", -- & => better [ ampersand ] symbol
+    -- "ss09", -- >>= <<= ||= |=
+    -- "cv25", -- .-
+    -- "cv26", -- :-
+    -- "cv32", -- .=
+    -- "cv27", -- []
+    -- "cv28", -- {. .}
+    -- "ss07", -- =~ !~
+  },
 })
 -- TODO: add backup font
 config.font_size = 18
 config.send_composed_key_when_left_alt_is_pressed = false -- left opt is ALT/META key
 config.send_composed_key_when_right_alt_is_pressed = true -- right opt is normal (so i can type #)
 
--- disable_default_key_bindings = true
+config.disable_default_key_bindings = true
 
 config.keys = {
-	-- use SUPER + [ = | - | 0 ] to change font size
-	-- this frees up CTRL for neovim keybinds
-	{
-		key = "=",
-		mods = "CTRL",
-		action = wezterm.action.DisableDefaultAssignment,
-	},
-	{
-		key = "-",
-		mods = "CTRL",
-		action = wezterm.action.DisableDefaultAssignment,
-	},
-	{
-		key = "0",
-		mods = "CTRL",
-		action = wezterm.action.DisableDefaultAssignment,
-	},
+  {
+    key = "=",
+    mods = "SUPER",
+    action = act.IncreaseFontSize,
+  },
+  {
+    key = "-",
+    mods = "SUPER",
+    action = act.DecreaseFontSize,
+  },
+  {
+    key = "0",
+    mods = "SUPER",
+    action = act.ResetFontSize,
+  },
+  {
+    key = "|",
+    mods = "SUPER",
+    action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+  },
+  {
+    key = "-",
+    mods = "SUPER|SHIFT",
+    action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
+  },
+  {
+    key = "t",
+    mods = "SUPER",
+    action = act.SpawnTab("CurrentPaneDomain"),
+  },
+  {
+    key = "w",
+    mods = "SUPER",
+    action = act.CloseCurrentTab({ confirm = true }),
+  },
+  {
+    key = "c",
+    mods = "SUPER",
+    action = act.CopyTo("Clipboard"),
+  },
+  {
+    key = "v",
+    mods = "SUPER",
+    action = act.PasteFrom("Clipboard"),
+  },
+  {
+    key = "]",
+    mods = "SUPER",
+    action = act.ActivateTabRelative(1),
+  },
+  {
+    key = "[",
+    mods = "SUPER",
+    action = act.ActivateTabRelative(-1),
+  },
 }
 
 -- launch maximised
 wezterm.on("gui-startup", function(cmd)
-	---@diagnostic disable: unused-local
-	-- selene: allow(unused_variable)
-	-- local tab, pane, window = mux.spawn_window(cmd or {})
-	local _, _, window = mux.spawn_window(cmd or {})
-	window:gui_window():maximize()
+  ---@diagnostic disable: unused-local
+  -- selene: allow(unused_variable)
+  -- local tab, pane, window = mux.spawn_window(cmd or {})
+  local _, _, window = mux.spawn_window(cmd or {})
+  window:gui_window():maximize()
 end)
 
 return config
