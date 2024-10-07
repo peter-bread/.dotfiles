eb9b0175ece88f8a86860fd85241e8efc257a689
Peter Sheehan <petersheehan2003@gmail.com>
2024-10-07 17:13:26 +0100

    feat(zsh): always show tab bar

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,7 +6,7 @@ local config = wezterm.config_builder()
 
 -- Window ---------------------------------------------------------------------
 
-config.window_decorations = "INTEGRATED_BUTTONS"
+config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
 config.window_background_opacity = 0.75
 config.macos_window_background_blur = 10
 
@@ -20,7 +20,7 @@ end)
 
 -- Tabs -----------------------------------------------------------------------
 
-config.hide_tab_bar_if_only_one_tab = true
+config.hide_tab_bar_if_only_one_tab = false
 
 config.window_frame = {
   font = wezterm.font({
