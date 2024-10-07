0cbfb47051483c86bfc5a7d1430fcfc534a4425f
Peter Sheehan <petersheehan2003@gmail.com>
2024-10-07 19:34:20 +0100

    fix(zsh): remove unneeded option

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,7 +6,7 @@ local config = wezterm.config_builder()
 
 -- Window ---------------------------------------------------------------------
 
-config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+config.window_decorations = "INTEGRATED_BUTTONS"
 config.window_background_opacity = 0.75
 config.macos_window_background_blur = 10
 
