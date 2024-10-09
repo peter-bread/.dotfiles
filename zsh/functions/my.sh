9bece2ed0224e3355af0c1744be187cf54024033
Peter Sheehan <petersheehan2003@gmail.com>
2024-10-09 16:01:23 +0100

    fix(zsh): keep help function hidden

--- zsh/functions/my.sh
+++ zsh/functions/my.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env bash
 
-function _show_help_my() {
-  echo -e "Go to \e[1mmy\e[0m important places."
-  echo
-  echo -e "\e[1;4mUsage\e[0m: my <place>"
-  echo
-  echo -e "\e[1;4mPlaces\e[0m:"
-  echo "  d, dots, dotfiles     cd to \$DOTFILES"
-  echo "  n, nvim, neovim       cd to nvim config"
-}
-
 function my() {
+
+  function _show_help_my() {
+    echo -e "Go to \e[1mmy\e[0m important places."
+    echo
+    echo -e "\e[1;4mUsage\e[0m: my <place>"
+    echo
+    echo -e "\e[1;4mPlaces\e[0m:"
+    echo "  d, dots, dotfiles     cd to \$DOTFILES"
+    echo "  n, nvim, neovim       cd to nvim config"
+  }
+
   if [[ -z $1 ]]; then
     _show_help_my
     return 1
   fi
+
   case $1 in
   h | -h | help | --help)
     _show_help_my
