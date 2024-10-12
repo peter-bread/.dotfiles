7931711cac2d908ffc76ce802ab661af6dd8dae8
Peter Sheehan <petersheehan2003@gmail.com>
2024-10-12 18:23:07 +0100

    feat(zsh): updated `bak` function (unfinished)

--- zsh/functions/backup.sh
+++ zsh/functions/backup.sh
@@ -1,11 +1,60 @@
 #!/usr/bin/env bash
 
-bak() {
-  if [[ "$1" == "-r" || $1 == "--restore" ]]; then
-    mv "$2"{.bak,}
-  elif [[ "$1" == "-h" || $1 == "--help" ]]; then
-    echo "help info lol"
-  else
-    mv "$1"{,.bak}
+function bak() {
+  restore=0
+  command="mv"
+
+  while [[ $1 == --* || $1 == -? ]]; do
+    case $1 in
+    -c | --copy)
+      command="cp"
+      shift
+      ;;
+    -r | --restore)
+      restore=1
+      shift
+      ;;
+    *)
+      echo "Unknown option"
+      return 1
+      ;;
+    esac
+  done
+
+  # If not restoring, do not allow the copy option when creating a backup
+  if [[ $restore -eq 1 && $command == "cp" ]]; then
+    echo "Error: Cannot use --copy when restoring backups."
+    return 1
   fi
+
+  for file in "$@"; do
+    if [[ $restore -eq 1 ]]; then
+      if [[ $file == *.bak ]]; then
+        file_bak="${file%.bak}"
+      else
+        echo "Doesn't end in .bak. Skipping..."
+        continue
+      fi
+
+      # Check if the target file already exists
+      if [[ -e $file_bak ]]; then
+        echo "Warning: '$file_bak' already exists."
+        echo 'Would you like to overwrite (y/N)'
+        read -r answer
+        if [[ ! "$answer" =~ ^[Yy]$ ]]; then
+          echo "Skipping restoration of '$file'."
+          continue
+        fi
+      fi
+    else
+      file_bak="${file}.bak"
+    fi
+
+    if "$command" "$file" "$file_bak"; then
+      echo "Processed: $file -> $file_bak"
+    else
+      echo "Error processing: $file"
+    fi
+  done
+
 }
