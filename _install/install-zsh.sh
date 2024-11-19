58abb778e8f7ccc04a9ee169b32bc5a8b7c7930a
Peter Sheehan <petersheehan2003@gmail.com>
2024-11-19 14:45:07 +0000

    feat(zsh): add custom completions here

--- _install/install-zsh.sh
+++ _install/install-zsh.sh
@@ -6,3 +6,8 @@ mkdir -p "$ZSH_COMPLETIONS"
 ln -sf "$DOTFILES/zsh/zshenv" "$HOME/.zshenv"
 ln -sf "$DOTFILES/zsh/zshrc" "$ZDOTDIR/.zshrc"
 ln -sf "$DOTFILES/zsh/zprofile" "$ZDOTDIR/.zprofile"
+
+# link custom zsh completions
+for comp in "$DOTFILES"/zsh/completions/*; do
+  ln -sf "$comp" "$ZSH_COMPLETIONS/$(basename "$comp")"
+done
