baebf4443159211c7ec27585750cdc82629c7ad3
Peter Sheehan <petersheehan2003@gmail.com>
2024-12-17 22:47:40 +0000

    chore: delete old files

--- zsh/ruby.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-export PATH="/opt/homebrew/lib/ruby/gems/3.3.0/bin:$PATH"
-export PATH="/opt/homebrew/opt/ruby/bin:$PATH"
-export LDFLAGS="-L/opt/homebrew/opt/ruby/lib"
-export CPPFLAGS="-I/opt/homebrew/opt/ruby/include"
