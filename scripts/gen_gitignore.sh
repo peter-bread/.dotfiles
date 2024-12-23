b59d0cae0c6866fc04b84b3677fd5787e4dad3dd
Peter Sheehan <petersheehan2003@gmail.com>
2024-12-23 21:11:28 +0000

    docs(scripts): add description to script

--- scripts/gen_gitignore.sh
+++ scripts/gen_gitignore.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# Generate a new gitignore using global gitignore as a template
+
 ignore=$(git config --global core.excludesfile)
 
 [[ -n $ignore ]] && cat "${ignore/#\~/"$HOME"}" >.gitignore
