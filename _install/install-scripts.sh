a237c550f7d630f4eea2907885bfdc29f0d2c82f
Peter Sheehan <petersheehan2003@gmail.com>
2024-12-26 23:50:23 +0000

    fix(scripts): only link `.sh` files; not README.md

--- _install/install-scripts.sh
+++ _install/install-scripts.sh
@@ -2,6 +2,6 @@
 
 mkdir -p "$HOME/.local/bin"
 
-for script in "$DOTFILES"/scripts/*; do
+for script in "$DOTFILES"/scripts/*.sh; do
   ln -sf "$script" "$HOME/.local/bin/$(basename "${script%.*}")"
 done
