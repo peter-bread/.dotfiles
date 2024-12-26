a4f17153b71a3147671755baf9e7049ede5b0eae
Peter Sheehan <petersheehan2003@gmail.com>
2024-12-26 23:46:45 +0000

    feat(scripts): add yamlify
    This is a simple script to ensure consisten YAML extensions.

---
+++ scripts/yamlilfy.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# Script to ensure consistent YAML extensions in a directory
+
+set -Eeuo pipefail
+
+show_help() {
+  echo "Ensure consistent YAML extensions"
+  echo
+  echo "Usage: $0 <yml|yaml>"
+  echo
+  echo "  - yml: convert all .yaml files to .yml"
+  echo "  - yaml: convert all .yml files to .yaml"
+  exit 1
+}
+
+if [[ $# -ne 1 ]]; then
+  show_help
+fi
+
+if [[ $1 != "yml" && $1 != "yaml" ]]; then
+  show_help
+fi
+
+case $1 in
+yml)
+  fd -e yaml -x mv "{}" "{.}.yml"
+  ;;
+yaml)
+  fd -e yml -x mv "{}" "{.}.yaml"
+  ;;
+esac
