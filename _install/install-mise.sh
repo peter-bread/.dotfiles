1e970a090a951a3f7462567ac6647fe7fd4a2178
Peter Sheehan <petersheehan2003@gmail.com>
2025-01-22 18:10:52 +0000

    feat(mise): start ubuntu setup

--- _install/install-mise.sh
+++ _install/install-mise.sh
@@ -13,6 +13,35 @@ Darwin)
   ;;
 
 Linux)
-  # TODO: distinguish between different types of Linux
+
+  # use os-release file when possible
+  if [[ -f /etc/os-release ]]; then
+    id=$(grep --color=never -ioP '^ID=\K.+' /etc/os-release)
+
+    case $id in
+    ubuntu)
+      [ -f "$DOTFILES/mise/ubuntu.toml" ] &&
+        ln -sf "$DOTFILES/mise/ubuntu.toml" "$XDG_CONFIG_HOME/mise/conf.d/ubuntu.toml"
+
+      # WARN:
+      # do I want to be installing software here?
+      # Is this just for symlinking files?
+      # Is this now becoming more bootstrappy?
+
+      # install dependencies:
+
+      # ruby build
+      # sudo apt-get install autoconf patch build-essential rustc libssl-dev \
+      #   libyaml-dev libreadline6-dev zlib1g-dev libgmp-dev libncurses5-dev \
+      #   libffi-dev libgdbm6 libgdbm-dev libdb-dev uuid-dev -y
+
+      # lua
+      # sudo apt-get install unzip -y
+      ;;
+    esac
+  fi
+
+  # TODO: distinguish between different types of Linux when /etc/os-release doesn't exist
+
   ;;
 esac
