b7277dce45d7f2681737230fd4e963e7e45bd6a0
Peter Sheehan <145384599+peter-bread@users.noreply.github.com>
2025-10-04 01:40:49 +0100

    fix(find_links): search for launch agents to get skhd service plist

--- find_links.sh
+++ find_links.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
-# find / delete symlinks from $DOTFILES
-# assumes certain environment variables are set
+# Find / delete symlinks from $DOTFILES.
+# Assumes certain environment variables are set.
 
-exit 0
+# exit 0
 
-dirs=("$HOME" "$XDG_CONFIG_HOME" "$ZSH_COMPLETIONS" "$HOME"/.local/bin)
-depths=(1 999 1 1)
+dirs=("$HOME" "$XDG_CONFIG_HOME" "$ZSH_COMPLETIONS" "$HOME"/.local/bin "$HOME/Library/LaunchAgents")
+depths=(1 999 1 1 1)
 
 for i in "${!dirs[@]}"; do
   find "${dirs[i]}" -maxdepth "${depths[i]}" -type l -lname "$DOTFILES/*" # -delete
 done
+
+# find "$HOME" -maxdepth 999 -type l -lname "$DOTFILES/*" # -delete
