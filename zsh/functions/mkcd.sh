7c663ca895e73d313148e65fbba4b97b6b95b34a
Peter Sheehan <145384599+peter-bread@users.noreply.github.com>
2025-10-24 18:36:32 +0100

    refactor(zsh)!: move functions into single file
    Also delete unneeded functions and functions that should be
    executable scripts

--- zsh/functions/mkcd.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# create new directory and cd into it
-
-function mkcd() {
-  builtin mkdir "$@" && builtin cd
-}
--- zsh/functions/network.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-function get_wifi_ip_addr() {
-  ipconfig getifaddr en0
-}
-
-function get_wifi_ip_addr2() {
-  networksetup -getinfo Wi-Fi | rg "^IP address:" | awk '{print $3}'
-}
